458c23cc735ca7f0b83180804b81f168c5486333
Yasin Husen <yhusen636@gmail.com>
2025-10-26 13:17:08 +0300

    testimonial1

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
   document.body.classList.remove("show-mobile-menu");
 });
+// initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // responsive breakpoint
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
